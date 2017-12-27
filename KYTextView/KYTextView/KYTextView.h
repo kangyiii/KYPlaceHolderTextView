c323fedec14b993fdb830c777ac723a6a239f6d7
KangYi <kangyi_mm@163.com>
2017-12-27 10:47:08 +0800

    first commit

---
+++ KYTextView/KYTextView/KYTextView.h
@@ -0,0 +1,13 @@
+//
+//  KYTextView.h
+//  KYTextView
+//
+//  Created by 康义 on 2017/12/27.
+//  Copyright © 2017年 康义. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KYTextView : UITextView
+
+@end
---
+++ KYTextView/KYTextView/KYTextView.m
@@ -0,0 +1,21 @@
+//
+//  KYTextView.m
+//  KYTextView
+//
+//  Created by 康义 on 2017/12/27.
+//  Copyright © 2017年 康义. All rights reserved.
+//
+
+#import "KYTextView.h"
+
+@implementation KYTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
